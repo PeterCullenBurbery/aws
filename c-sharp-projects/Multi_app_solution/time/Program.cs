baf01ae2b507a5791e4e53f4f806225ee8a2ab0d
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 10:28:03 -0400

    Add milliseconds to timestamp output
    The timestamp format now includes milliseconds as a three-digit padded value, improving precision for time-based outputs.

--- c-sharp-projects/Multi_app_solution/time/Program.cs
+++ c-sharp-projects/Multi_app_solution/time/Program.cs
@@ -8,8 +8,8 @@ class Program
     {
         DateTime now = DateTime.Now;
 
-        // Format as YYYY_MMM_DDD_HHH_MMM_SSS with 3-digit padding for all except year
-        string formattedTimestamp = $"{now:yyyy}_{now.Month:D3}_{now.Day:D3}_{now.Hour:D3}_{now.Minute:D3}_{now.Second:D3}";
+        // Format as YYYY_MMM_DDD_HHH_MMM_SSS_mmm
+        string formattedTimestamp = $"{now:yyyy}_{now.Month:D3}_{now.Day:D3}_{now.Hour:D3}_{now.Minute:D3}_{now.Second:D3}_{now.Millisecond:D3}";
 
         Console.WriteLine(formattedTimestamp);
     }
