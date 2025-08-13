fc7be5ddd6dc2aeed8316cd039c69814b777f17f
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 10:25:53 -0400

    Update timestamp formatting and output
    Clarified comment to specify 3-digit padding for all timestamp components except year. Changed console output to print only the formatted timestamp.

--- c-sharp-projects/Multi_app_solution/time/Program.cs
+++ c-sharp-projects/Multi_app_solution/time/Program.cs
@@ -8,9 +8,9 @@ class Program
     {
         DateTime now = DateTime.Now;
 
-        // Format as YYYY_MMM_DDD_HHH_MMM_SSS with three digits for all except year
+        // Format as YYYY_MMM_DDD_HHH_MMM_SSS with 3-digit padding for all except year
         string formattedTimestamp = $"{now:yyyy}_{now.Month:D3}_{now.Day:D3}_{now.Hour:D3}_{now.Minute:D3}_{now.Second:D3}";
 
-        Console.WriteLine("Current Timestamp: " + formattedTimestamp);
+        Console.WriteLine(formattedTimestamp);
     }
 }
