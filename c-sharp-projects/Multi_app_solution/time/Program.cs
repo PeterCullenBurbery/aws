cac7eb0c7980207127afaa45804d7c481ef5441c
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-08-13 10:28:59 -0400

    Add microseconds to timestamp formatting
    Updated the timestamp format in Program.cs to include microseconds, improving time precision. The formatted output now uses microseconds instead of milliseconds.

--- c-sharp-projects/Multi_app_solution/time/Program.cs
+++ c-sharp-projects/Multi_app_solution/time/Program.cs
@@ -8,8 +8,12 @@ class Program
     {
         DateTime now = DateTime.Now;
 
-        // Format as YYYY_MMM_DDD_HHH_MMM_SSS_mmm
-        string formattedTimestamp = $"{now:yyyy}_{now.Month:D3}_{now.Day:D3}_{now.Hour:D3}_{now.Minute:D3}_{now.Second:D3}_{now.Millisecond:D3}";
+        // Get microseconds from ticks
+        long microseconds = now.Ticks % TimeSpan.TicksPerSecond / 10;
+
+        // Format: YYYY_MMM_DDD_HHH_MMM_SSS_UUUUUU
+        string formattedTimestamp =
+            $"{now:yyyy}_{now.Month:D3}_{now.Day:D3}_{now.Hour:D3}_{now.Minute:D3}_{now.Second:D3}_{microseconds:D6}";
 
         Console.WriteLine(formattedTimestamp);
     }
